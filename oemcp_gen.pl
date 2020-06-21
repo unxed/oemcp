da699d6dada602cf9526f7fc085e6810fa8b110c
unxed <unxed@mail.ru>
2020-06-21 22:51:29 +0300

    Add files via upload

---
+++ oemcp_gen.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+open(FH, '>', 'oemcp.sh') or die $!;
+
+print FH "#!/bin/bash\n";
+print FH "lang=\$(locale | grep \"LANGUAGE\" | sed -n -e 's/^LANGUAGE=//p')\n";
+
+$dir = ".";
+opendir DIR, $dir;
+my @dir = readdir(DIR);
+close DIR;
+
+##print join(", ", @parts),"\n";
+
+foreach(sort @dir){
+    if (-f $dir . "/" . $_ ) {
+        my @parts = split "\\.", $_;
+        if (@parts[1] eq 'nls') {
+            #print $_, "\n";
+            open(my $file, '<', $dir . "/" . $_) or die "Unable to open $!";
+            my @strings=<$file>;
+            foreach(@strings) {
+                my ( @match1 ) = ($_ =~ /(LOCALE_IDEFAULTCODEPAGE "(\d+)")/g);
+                if ($match1[1]) { $cp = $match1[1]; }
+                my ( @match2 ) = ($_ =~ /(LOCALE_SISO3166CTRYNAME "(\w+)")/g);
+                if ($match2[1]) { $cn = $match2[1]; }
+                my ( @match3 ) = ($_ =~ /(LOCALE_SISO639LANGNAME "(\w+)")/g);
+                if ($match3[1]) { $ln = $match3[1]; }
+                #  LOCALE_IDEFAULTCODEPAGE "850"
+                #  LOCALE_SISO3166CTRYNAME "ZA"
+                #  LOCALE_SISO639LANGNAME "af"
+            }
+            if (($cn ne '029') && ($cp ne '1')) {
+                print "$ln", "_", "$cn - $cp\n";
+                print FH 'if [ "$lang" = "', $ln, '_', $cn, '" ]; then', "\n";
+                print FH "echo '$cp'\n";
+                print FH "fi\n";
+            }
+        }
+    }
+}
+
+close(FH);
+
+chmod 0755, 'oemcp.sh';
